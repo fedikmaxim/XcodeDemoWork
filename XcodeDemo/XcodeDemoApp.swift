0437d28e16eba49faed2d8c96ac19f896523f9a2
Maxim <fedykmaxim@gmail.com>
2023-03-22 10:05:54 +0200

    Initial Commit

---
+++ XcodeDemo/XcodeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoApp.swift
+//  XcodeDemo
+//
+//  Created by Fedyk Maksym on 22.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct XcodeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
