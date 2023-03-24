e6336d5aa49d8af79a9b74f5df909b54522e1ed2
Maxim <fedyk.m@isitlab.com>
2023-03-24 12:35:42 +0200

    commitlast1

--- XcodeDemo/ContentView.swift
+++ XcodeDemo/ContentView.swift
@@ -9,56 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
+
         ZStack {
-            
-            Color(.systemMint)
-                .ignoresSafeArea()
-            VStack(alignment: .leading, spacing: 20.0) {
+            VStack(alignment: .leading, spacing: 30) {
+                
                 Image("niagara1")
-                     .resizable()
-                     .aspectRatio(contentMode: .fit)
-                     .cornerRadius(15)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                
+                
+                
                 HStack {
                     Text("Niagara Falls")
-                        .font(.title)
+                        .font(.title2)
                         .fontWeight(.bold)
+                    
                     Spacer()
+                    
+                        
                     VStack {
                         HStack {
-                                Image(systemName: "star.fill")
-                                Image(systemName: "star.fill")
-                                Image(systemName: "star.fill")
-                                Image(systemName: "star.fill")
-                                Image(systemName:
-                                        "star.leadinghalf.filled")
-                                
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
                         }
-                        Text("(Reviews 361)")
-                        
+                        Text("Reviews 361")
+                            .fontWeight(.light)
+                            
                     }
                     .foregroundColor(.orange)
-                    .font(.caption)
-                }          
+                    
+                    
+                }
+        
+                Text("Wellcome to niagara falls. You can see many beautifull piace of the world.")
+                    .multilineTextAlignment(.leading)
+                    
                 
-                Text("Come visit the fallsfor an experience of a lifetime")
-                HStack{
+                HStack(alignment: .top, spacing: 10) {
                     Spacer()
-                    Image(systemName: "fork.knife")
-                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "car.fill")
+                    Image(systemName: "car.fill")
                 }
                 .foregroundColor(.gray)
                 .font(.caption)
+                
             }
-            .padding()
-            .background(Rectangle()
-                .foregroundColor(.white))
-                .cornerRadius(15)
-                .shadow(radius: 15)
-            .padding()
+            
+            
+            
         }
-        
-        
+        .padding()
+        .background(Rectangle() .foregroundColor(.white))
+        .cornerRadius(15)
+        .shadow(radius: 5)
+        .padding()
         
         
     }
